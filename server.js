d9a15299eb8669bf48d126132a12e2d38db62099
zidan hairra ramadhan <zidanramadhan950@gmail.com>
2025-10-27 20:03:05 +0700

    v

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ async function startServer() {
     const PORT = process.env.PORT || 3000;
 
     // === 1. KONFIGURASI KONEKSI DATABASE ===
-    const db = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
+   const db = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+        // (Tidak perlu 'ssl: false', karena default-nya sudah false)
+   });
 
     // === 2. MEMBUAT SEMUA TABEL TERLEBIH DAHULU ===
     // Membuat tabel untuk produk
